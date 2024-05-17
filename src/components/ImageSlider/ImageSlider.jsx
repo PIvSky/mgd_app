f85311292a44e4ea86ac04fba14eda9198ff4ebe
PIvSky <piotr.iwansky@gmail.com>
2024-05-17 10:20:15 +0200

    Added arrows for slider in mobile

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -1,26 +1,36 @@
 import './ImageSlider.scss'
 import { insertLineBreaks } from '../../util/utility'
+import arrowMobile from '../../assets/arrow/arrow-mobile.png'
+import arrowDesktop from '../../assets/arrow/arrow-desktop.png'
 
 const ImageSlider = ({data}) => {
 
     return (
         <div className='slider-container'>
-                <div className='slide'>
-                    <h2 className='slide-title'>{data.title}</h2>
-                    <div className='slide-img-wrapper'>
-                        <div className='slide-img'>
-                            <div className='slide-img__logo'>
-                                <img src={data.logoSrc} alt='brand-logo'></img>
-                            </div>
-                            <img className='slide-img__element' src={data.imgSrc} alt='brand-img' />
-                        </div>
+            
+            <div className='slide'>
+                
+                <h2 className='slide-title'>{data.title}</h2>
+                <div className='slide-img-wrapper'>
+                    <div className='left-arrow-mobile'>
+                        <img src={arrowMobile} alt='left-arrow'></img>
                     </div>
-                    
-                    <p className='slide-content'>{data.content}</p>
-                    <p className='slide-sign'>{insertLineBreaks(data.sign)}</p>
+                    <div className='slide-img'>
+                        <div className='slide-img__logo'>
+                            <img src={data.logoSrc} alt='brand-logo'></img>
+                        </div>
+                        <img className='slide-img__element' src={data.imgSrc} alt='brand-img' />
+                    </div>
+                    <div className='right-arrow-mobile'>
+                        <img src={arrowMobile} alt='right-arrow'></img>
+                    </div>
                 </div>
-                <div className='dots'></div>
+                <p className='slide-content'>{data.content}</p>
+                <p className='slide-sign'>{insertLineBreaks(data.sign)}</p>
+                
             </div>
+            <div className='dots'></div>
+        </div>
     )
 }
 
