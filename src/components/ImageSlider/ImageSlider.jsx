5e371551e0067405d3b77c0129edf6d1e68bc025
PIvSky <piotr.iwansky@gmail.com>
2024-05-20 17:22:15 +0200

    done succes_desktop for tablet and desktop screen

--- src/components/ImageSlider/ImageSlider.jsx
+++ src/components/ImageSlider/ImageSlider.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './ImageSlider.scss'
 import { insertLineBreaks } from '../../util/utility'
 import arrowMobile from '../../assets/arrow/arrow-mobile.png'
-// import arrowDesktop from '../../assets/arrow/arrow-desktop.png'
+import arrowDesktop from '../../assets/arrow/arrow-desktop.png'
 
 const ImageSlider = ({data}) => {
 
@@ -29,11 +29,14 @@ const ImageSlider = ({data}) => {
         <div className='slider-container'>           
             <div className='slide'>
                 <div className='slide-img-wrapper'>
-                    <h2 className='slide-title'>{data[currentIndex].title}</h2>
-                    <div className='left-arrow-mobile' onClick={prevSlide}>
-                        <img src={arrowMobile} alt='left-arrow'></img>
-                    </div>
+                    <h2 className='slide-title'>{data[currentIndex].title}</h2>              
                     <div className='slide-img'>
+                        <div className='left-arrow-desktop' onClick={prevSlide}>
+                            <img src={arrowDesktop} alt='left-arrow'></img>
+                        </div>
+                        <div className='left-arrow-mobile' onClick={prevSlide}>
+                            <img src={arrowMobile} alt='left-arrow'></img>
+                        </div>
                         <div className='slide-img__logo'>
                             <img 
                                 src={data[currentIndex].logoSrc} 
@@ -45,23 +48,28 @@ const ImageSlider = ({data}) => {
                             src={data[currentIndex].imgSrc} 
                             alt='brand-img'>
                         </img>
-                    </div>
-                    <div className='dots-container'>
-                    {data.map((item ,index) => (
-                        <div className='dots-container__items' key={index} onClick={() => currentSlide(index)}>
-                            <svg height="20" width="20" onClick={prevSlide}>
-                                <circle className='dot-image' cx="7" cy="7" r="7" fill={currentIndex === index ? '#000' : '#D9D9D9'} />
-                            </svg> 
-                        </div>
-                    ))}
-                    </div>
-                    <div className='right-arrow-mobile' onClick={nextSlide}>
-                        <img src={arrowMobile} alt='right-arrow'> 
+                        <div className='right-arrow-mobile' onClick={nextSlide}>
+                            <img src={arrowMobile} alt='right-arrow'> 
                         </img>
                     </div>
-                </div>
-                <p className='slide-content'>{data[currentIndex].content}</p>
-                <p className='slide-sign'>{insertLineBreaks(data[currentIndex].sign)}</p>            
+                    </div>
+                        <div className='dots-container'>
+                        {data.map((item ,index) => (
+                            <div className='dots-container__items' key={index} onClick={() => currentSlide(index)}>
+                                <svg height="20" width="20" onClick={prevSlide}>
+                                    <circle className='dot-image' cx="7" cy="7" r="7" fill={currentIndex === index ? '#000' : '#D9D9D9'} />
+                                </svg> 
+                            </div>
+                        ))}
+                        </div>
+                    <div className='slide-content'>
+                        
+                        <p className='slide-content-element'>{data[currentIndex].content} <div className='right-arrow-desktop' onClick={prevSlide}>
+                            <img src={arrowDesktop} alt='left-arrow'></img>
+                        </div></p>
+                        <p className='slide-content-sign'>{insertLineBreaks(data[currentIndex].sign)}</p>
+                    </div>                    
+                </div>        
             </div>
         </div>
     )
