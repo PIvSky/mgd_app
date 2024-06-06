b75f66d0d04c14f2b91b25425232a0911e92ffa8
PIvSky <piotr.iwansky@gmail.com>
2024-06-06 21:31:41 +0200

    added JSX and data structure to ExistBiz.jsx

--- src/components/Offer/ExistBiz/ExistBiz.jsx
+++ src/components/Offer/ExistBiz/ExistBiz.jsx
@@ -1,11 +1,105 @@
-import './ExistBiz.scss'
+import './ExistBiz.scss';
+import VectorBg from '../../Assets/VectorBg/VectorBg';
+import sketch from '../../../assets/offer/new-biz/sketch.png';
 
 const ExistBiz = () => {
-    return (
-        <>
-            <h4>Dla istniejących</h4>
 
-        </>
+    const main = {
+        title: 'Dla istniejących biznesów.',
+        rectangle: 'Dzięki wiedzy na temat trendów branżowych i zdolności do dostosowywania strategii do przyszłego sukcesu będziemy dla ciebie wszechstronnym partnerem oferując szeroki zakres usług, które pomogą ci nawigować w zawiłościach branży, dostosować się do zmian i tworzyć trwałą wartość',
+    };
+
+    const research = {
+        title: 'Przejdziemy proces dokładnej diagnozy i badanaia potrzeb, analizując m.in:',
+        subtitleOne: 'Działalność biznesową',
+        subtitleTwo: 'Działalność marketingową',
+        businessList : [
+            'Diagnoza obecnego stanu sprzedaży',
+            'Praca z klientami - weryfikacja obecnych standardów obsługi i budowa nowych, optymalnych; weryfikacja i ocena scoringowa oferty całościowego menu',
+            'Ewaluacja, badanie kondycji sprzedaży i dokumentów sprzedaży: raport wraz z oceną',
+            'Ewaluacja, badanie lokalizacji (usytuowanie, wystrój, ułożenie, prezentacja produktów itp.)',
+            'Badanie elastyczności cenowej w danej kategorii (np. restauracja) i segmencie (np. mainstream, fine dining, średnia półka)',
+            'Analiza kadr – płace, kompetencje i efektywność sprzedażowa',
+            'Rekrutacja i szkolenie kadr',
+            'Analiza kosztów i rentowności klienta (FC_BC_LC)',
+            'Analiza wydajności pracowników',
+            'Analiza gospodarki magazynowej'
+        ],
+        marketingList : [
+            'Ocena jakości i efektywności kanałów komunikacji, ocena konsumenckiej percepcji marki i produktu',
+            'Ocena brandingu'
+        ],
+    }
+
+    const benefits = {
+        subtitle: 'Jakie bezpośrednie korzyści otrzymasz we współpracując z nami:',
+        nestedSubtitleOne: 'Operacyjne:',
+        nestedSubtitleTwo: 'Marketingowe:',
+        listOne : [
+            'Rozwiązywanie bieżących problemów',
+            'Wzmocnienie i rozwój sprzedaży',
+            'Budowa optymalnych standardów obsługi',
+            'Wdrożenie i wskazania praktyczne z ewaluacji i badania kondycji sprzedaży',
+            'Efektywna polityka cenowa',
+            'Wskazania praktyczne z analizy kosztów i rentowności klienta (FC_BC_LC)',
+            'HACCP, GHP, GMP, BHP',
+            'Wdrożenie narzędzi controllingowych i Business Intelligence'
+        ],
+        listTwo : [
+            'Branding, Tone of Voice, strategia komunikacji',
+            'Projekt menu'
+        ]
+    }
+
+    return (
+        <div className='ex-biz'>
+            <div className='main-container'>
+                <h3 className='title'>{main.title}</h3>
+                <div className='rectangle-container'>
+                    <div className='rectangle'>
+                        <p className='rectangle-content'>{main.rectangle}</p>
+                    </div>
+                </div>
+            </div>
+            <div className='research'>
+                <h3 className='research-title'>{research.title}</h3>
+                <div className='research-subtitle'>
+                    <img src={sketch} alt='sketch' />
+                    <p>{research.subtitleOne}</p>
+                </div>
+                <p className='research-nested-subtitle-biz'>{research.subtitleOne}</p>
+                <div className='research-list-one'>
+                    {/* tu będą kafelki */}
+                </div>
+                <p className='research-nested-subtitle-mark'>{research.subtitleOne}</p>
+                <div className='research-list-two'>
+                    {/* tu będą kafelki */}
+                </div>
+            </div>                
+            <div className='benefits'>
+                <div className='subtitle-two'>
+                    <img src={sketch} alt='sketch' />
+                    <p>{benefits.subtitle}</p>
+                </div>
+                <p className='nested-subtitle'>{benefits.nestedSubtitleOne}</p>
+                <div className='list-one'>
+                    <ul>
+                    {benefits.listOne.map((index, key)=> (
+                        <li key={key}>{index}</li>
+                    ))}
+                    </ul>
+                </div>
+                <p className='nested-subtitle'>{benefits.nestedSubtitleTwo}</p>
+                <div className='list-two'>
+                    <ul>
+                    {benefits.listTwo.map((index, key)=> (
+                        <li key={key}>{index}</li>
+                    ))}
+                    </ul>
+                </div>
+            </div>
+            <VectorBg className='bg-vector'/>
+        </div>
     )
 }
 
