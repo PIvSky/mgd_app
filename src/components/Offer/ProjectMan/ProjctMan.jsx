69fb608b9784f274a296a3e0bbb2ff5da425a287
PIvSky <piotr.iwansky@gmail.com>
2024-06-08 21:21:13 +0200

    added JSX layout of component

--- src/components/Offer/ProjectMan/ProjctMan.jsx
+++ src/components/Offer/ProjectMan/ProjctMan.jsx
@@ -1,11 +1,31 @@
-import './ProjectMan.scss'
+import './ProjectMan.scss';
+import VectorBg from '../../Assets/VectorBg/VectorBg';
 
 const ProjectMan = () => {
-    return (
-        <>
-            <h4>Dla project man</h4>
 
-        </>
+    const data = {
+        title: 'Project Management usług budowlano-wykończeniowych',
+        contentOne: 'Współpracujemy z doświadczonym generalnym wykonawcą, który świadczy wykończeniowo-budowlane dedykowane branży gastronomicznej, dostosowane do indywidualnych potrzeb.',
+        contentOne: 'Zespół specjalistów charakteryzuje się wieloletnim doświadczeniem w projektowaniu i realizacji lokali gastronomicznych i tworzy przestrzenie, które nie tylko spełniają funkcjonalne wymagania, ale również wyróżniają się estetyką i oryginalnością.',
+        contentOne: 'Zapewniamy realizację zadania od projektu po oddanie kluczy. Z pełnym spokojem i realnymi terminami.',
+        list : [
+
+        ]
+    }
+
+    return (
+        <div className='proman'>
+            <div className='proman-main'>
+                <h3 className='proman-main-title'></h3>
+                <p className='proman-main-content'></p>
+                <p></p>
+                <p></p>
+            </div>
+            <div className='proman-list'>
+
+            </div>
+            <VectorBg className='bg-vector'/>
+        </div>
     )
 }
 
