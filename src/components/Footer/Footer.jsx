461ab0993f854622d0c035d8383d9b15c94eba42
PIvSky <piotr.iwansky@gmail.com>
2024-06-17 15:34:56 +0200

    added contact data component in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './Footer.scss';
+import logoImg from '../../assets/logo/mika-logo-desktop.png';
 
 const Footer = () => {
 
@@ -23,12 +24,16 @@ const Footer = () => {
                 </div>
             </div>
             ) : (
-            <div className='contact'>
-
+            <div className='footer-info'>
+                <img className='footer-info-logo' src={logoImg} alt='logo-img'/>
+                <div className='footer-info-data'>
+                    <p>KONTAKT</p>
+                    <p>krzysztofmika@gmail.com</p>
+                    <p>+48 904 049 049</p>
+                </div>
+                <button className='footer-info-button'>polityka <br></br>prywatności</button>
             </div>
             )}
-            
-            
         </div>
     )
 }
