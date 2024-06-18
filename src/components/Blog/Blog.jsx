3f28d0eacdc4048c243c394fbeb3b09a7bd07fa3
PIvSky <piotr.iwansky@gmail.com>
2024-06-18 10:42:58 +0200

    added target content to Blog.jsx

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -5,13 +5,8 @@ const Blog = () => {
 
     const blogContent = [
         {
-            src: '',
-            title: 'Wywiad z podcastu',
-            content: "Zapraszamy do wysłuchania wywiadu, który Krzysztof udzielił Agnieszce Bukowskiej i Krzyśkowi Rzymanowi z Coffeespots Polska, w którym opowiada o tym jak ważne są finanse w prowadzeniu kawiarni."
-        },
-        {
-            src: '',
-            title: 'Wywiad z podcastu',
+            src: 'https://youtu.be/-WiFdLw0Z9w?si=smHVTBQ0vdHDffIO',
+            title: 'Kawiarnia to także Excel - Krzysztof Mika, Coffeedesk',
             content: "Zapraszamy do wysłuchania wywiadu, który Krzysztof udzielił Agnieszce Bukowskiej i Krzyśkowi Rzymanowi z Coffeespots Polska, w którym opowiada o tym jak ważne są finanse w prowadzeniu kawiarni."
         }
     ]
@@ -20,7 +15,16 @@ const Blog = () => {
             <div className='blog-content'>
                 {blogContent.map((item, index) => (
                     <div className='video-container' key={index}>
-                        <div className='video-frame'></div>
+                        <div className='video-frame'>
+                             <iframe 
+                                src="https://www.youtube.com/embed/-WiFdLw0Z9w?si=smHVTBQ0vdHDffIO" 
+                                title="YouTube video player" 
+                                frameborder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                referrerpolicy="strict-origin-when-cross-origin"
+                                loading="lazy"
+                                allowfullscreen></iframe>
+                        </div>
                         <h2 className='video-title'>{item.title}</h2>
                         <p className='video-content'>{item.content}</p>
                     </div>
